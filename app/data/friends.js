36a13af9a794b892011c4e966db912044bff6021
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-08-06 19:45:15 -0400

    commit 1

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendsArray = [{
+    "name": "Redmond",
+    "photo": "https://",
+    "scores": [
+        1,
+        5,
+        4,
+        2,
+        3,
+        5,
+        3,
+        1,
+        4,
+        2
+    ]
+}]
+
+
+
+
+
+
+
+module.exports = friendsArray;
