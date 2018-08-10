ea4e123effbf67a835e50518a29d2fd5634551f3
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-08-10 07:54:25 -0400

    commit 100

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,29 @@ module.exports = function(app)  {
 
   app.post("/api/friends", function(req, res) {
     
-});
+    var friendInput = req.body;
+    var friendResponses = friendInput.scores;
+
+    var matchName = "";
+    var matchImage = "";
+    var totalDifference = 10000;
+
+    for (var i = 0; i < friends.length; i++ ) {
+      var difference = 0;
+      for (var j = 0; j < friendResponses.length; j++ ) {
+        difference += Math.abs(friends[i].scores[j] - friendResponses[j]);
+      }
+      if (difference < totalDifference) {
+        totalDifference = difference;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    }
+
+    friends.push(friendInput);
+
+    res.json({status: "OK", matchName: matchName, matchImage: matchImage});
+  });
 };
 
 
