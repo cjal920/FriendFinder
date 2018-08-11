e8820244529a58996803571302f3f654da9931e9
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-08-11 16:20:35 -0400

    commit 110

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,177 +1,177 @@
 var friends = [{
-    name: "Johann Sebastian Bach",
-    photo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Johann_Sebastian_Bach.jpg",
+    name: "Princess Leia from The Empire Strikes Back",
+    photo: "https://overmental.com/wp-content/uploads/2015/09/leia-790x336.jpg",
     scores: [
-        1,
         5,
+        1,
+        2,
+        4,
+        4,
+        4,
+        4,
+        1,
+        4,
+        4
+    ]
+},
+{
+    name: "Fatima Blush from Never Say Never Again",
+    photo: "https://vignette.wikia.nocookie.net/villains/images/0/08/Fatima500full.jpg/revision/latest?cb=20170810231708&format=original",
+    scores: [
+        5,
+        2,
         4,
         2,
-        3,
+        1,
+        2,
+        5,
+        4,
+        1,
+        5
+    ]
+},
+{
+    name: "Caprica Six from Battlestar Galactica",
+    photo: "https://pbs.twimg.com/profile_images/55403527/165_400x400.jpg",
+    scores: [
+        5,
+        1,
         5,
         3,
-        1,
         4,
+        2,
+        5,
+        3,
+        2,
+        5
+    ]
+},
+{
+    name: "Hans Landa from Inglourious Basterds",
+    photo: "https://cdn-images-1.medium.com/max/1600/1*QD5ITcGI7uScAAC5c9u3VA.jpeg",
+    scores: [
+        1,
+        2,
+        4,
+        2,
+        1,
+        1,
+        5,
+        2,
+        1,
+        5
+    ]
+},
+{
+    name: "Martha from Who's Afraid of Virginia Woolf?",
+    photo: "https://static1.squarespace.com/static/56cdc4478259b5c112bb2285/t/58541a2d9f7456e64f2b7f04/1481906742325/?format=1500w",
+    scores: [
+        5,
+        5,
+        2,
+        2,
+        2,
+        5,
+        2,
+        5,
+        3,
         2
     ]
 },
 {
-    name: "Pablo Picasso",
+    name: "Khan Noonien Singh from Star Trek II: The Wrath of Khan",
+    photo: "https://i1.netflixmovies.com/dibsl9ebc/image/upload/w_1024/kexdkmbuebtyhxzyazfz.jpg",
+    scores: [
+        1,
+        1,
+        5,
+        1,
+        1,
+        1,
+        5,
+        1,
+        1,
+        5
+    ]
+},
+{
+    name: "Paul Atreides from Dune",
+    photo: "https://www.scifibloggers.com/wp-content/uploads/paul-atreides.jpg",
+    scores: [
+        1,
+        2,
+        4,
+        4,
+        5,
+        2,
+        5,
+        1,
+        4,
+        4
+    ]
+},
+{
+    name: "Jed Hill from Malice",
+    photo: "https://i.kinja-img.com/gawker-media/image/upload/sajymneoscr1njyinzk8.jpg",
+    scores: [
+        1,
+        4,
+        3,
+        4,
+        2,
+        2,
+        5,
+        5,
+        2,
+        5
+    ]
+},
+{
+    name: "Kelly Corcoran from The Heartbreak Kid",
+    photo: "https://1.bp.blogspot.com/-edxIIWX-mxs/VNDQGVzvlQI/AAAAAAAAFDk/_TtVQeIdQOw/s1600/The%2BHeartbreak%2BKid%2B4.JPG",
+    scores: [
+        5,
+        3,
+        3,
+        2,
+        1,
+        3,
+        4,
+        2,
+        4,
+        5
+    ]
+},
+{
+    name: "Mickey Sachs from Hannah and Her Sisters",
+    photo: "http://www.oscarchamps.com/wp-content/uploads/2016/08/1986-Hannah-and-her-Sisters-05.jpg",
+    scores: [
+        1,
+        2,
+        1,
+        4,
+        4,
+        5,
+        4,
+        3,
+        5,
+        2
+    ]
+},
+{
+    name: "Daniel Plainview from There Will Be Blood",
     photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
     scores: [
         1,
-        5,
         4,
+        4,
+        1,
+        1,
+        1,
+        5,
         2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Alexander the Great",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
         2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Eleanor Roosevelt",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Leonard Bernstein",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Johann Sebastian Bach",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Ho Chi Minh",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Eric Liddel",
-    photo: "https://robsview.files.wordpress.com/2013/04/eric-liddell.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Cleopatra",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "Charlie Parker",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
-    ]
-},
-{
-    name: "F. Scott Fitzgerald",
-    photo: "http://www.imagejournal.org/wp-content/uploads/2016/05/There-Will-Be-Blood.jpg",
-    scores: [
-        1,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        1,
-        4,
-        2
+        4
     ]
 },
 
